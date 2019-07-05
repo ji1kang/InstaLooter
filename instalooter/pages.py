c7d7bc69525f0fb002c962fddc3019413bd147b3
Jiwon <jiwonkang@hanyang.ac.kr>
2019-07-05 13:06:31 +0900

    feat: change request interval

--- instalooter/pages.py
+++ instalooter/pages.py
@@ -36,7 +36,7 @@ class PageIterator(typing.Iterator[typing.Dict[typing.Text, typing.Any]]):
     """
 
     PAGE_SIZE = 50
-    INTERVAL = randint(3, 10)
+    INTERVAL = randint(50, 60)
 
     _BASE_URL = "https://www.instagram.com/graphql/query/"
     _section_generic = NotImplemented    # type: Text
